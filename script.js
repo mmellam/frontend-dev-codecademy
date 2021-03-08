d0c6765f0677bc641d077795f81dca91f16a3002
mmellam <28260088+mmellam@users.noreply.github.com>
2021-03-08 12:42:18 +0100

    Added second part of message

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const sign = ["earth", "water", "fire", "wind"];
 const number = [2, 3, 4, 5];
 const action = ["lucky", "happy", "intriguing", "jolly"];
 const situation = ["meetings", "conversations", "encounters", "discussions"];
+const date = new Date().getDay();
 
 const randomizer = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const message = `Hello Stranger! Your sign is ${randomizer(sign)}. Your lucky number is ${randomizer(number)}.`;
+const message = `Hello Stranger! Your sign is ${randomizer(sign)}. Your lucky number is ${randomizer(number)}. 
+This means that I predict that you will have ${randomizer(number)} ${randomizer(action)} ${randomizer(situation)} today.`;
 console.log(message);
