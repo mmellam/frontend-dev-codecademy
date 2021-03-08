db1f533b1f47e7d33eb8493902dc6988c91c9cee
mmellam <28260088+mmellam@users.noreply.github.com>
2021-03-08 12:49:53 +0100

    added date function for year

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const sign = ["earth", "water", "fire", "wind"];
 const number = [2, 3, 4, 5];
 const action = ["lucky", "happy", "intriguing", "jolly"];
 const situation = ["meetings", "conversations", "encounters", "discussions"];
-const date = new Date().getDay();
+const date = new Date().getFullYear();
 
 const randomizer = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const message = `Hello Stranger! Your sign is ${randomizer(sign)}. Your lucky number is ${randomizer(number)}. 
-This means that I predict that you will have ${randomizer(number)} ${randomizer(action)} ${randomizer(situation)} today.`;
+const message = `Hello Stranger! 
+Your sign is ${randomizer(sign)}. 
+Your lucky number is ${randomizer(number)}. 
+This means that I predict that you will have ${randomizer(number)} ${randomizer(action)} ${randomizer(situation)} on this day in ${date}.`;
 console.log(message);
